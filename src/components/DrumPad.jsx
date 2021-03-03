8db5dc2abe4b2c11b803501bb24ffd7abe80b99a
Derek Price <derekp098@gmail.com>
2021-03-02 23:22:07 -0800

    testing lol

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react'
 
 const DrumPad = ({bankArr, index, setDisplay}) => {
 
+  var audio = new Audio('A.mp3');
+  audio.crossOrigin = 'anonymous';
+  // audio.src = 'A.mp3';
+
   useEffect(() => {
     document.addEventListener('keydown', (e) => {
       if (e.key.toUpperCase().charCodeAt(0) === bankArr[index].keyCode) {
@@ -11,17 +15,26 @@ const DrumPad = ({bankArr, index, setDisplay}) => {
   })
 
   const playSound = () => {
-    const sound = document.getElementById(bankArr[index].keyTrigger);
-    sound.currentTime = 0;
-    sound.play();
-    setDisplay(bankArr[index].id)
+    audio.currentTime = 0;
+    return audio.play();
+    // const sound = document.getElementById(bankArr[index].keyTrigger);
+    // sound.currentTime = 0;
+    // sound.play();
+    // setDisplay(bankArr[index].id)
   }
 
   return (
     <button
       className='drum-pad'
       id={bankArr[index].id}
-      onClick={playSound}
+      onClick={() => {
+        playSound().then(() => {
+          console.log('success');
+        }).catch(error => {
+          console.log('nope');
+          console.log(error);
+        })
+      }}
     >
       {bankArr[index].keyTrigger} 
       <audio 
