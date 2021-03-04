4bd3f04d383c34ecb5f54023d115d224e949e49c
Derek Price <derekp098@gmail.com>
2021-03-03 17:00:12 -0800

    removed a bunch of tester code that was not needed

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
-const DrumPad = ({bankArr, index, setDisplay}) => {
 
-  var audio = new Audio('A.mp3');
-  audio.crossOrigin = 'anonymous';
-  // audio.src = 'A.mp3';
+const DrumPad = ({bankArr, index, setDisplay}) => {
 
   useEffect(() => {
     document.addEventListener('keydown', (e) => {
@@ -15,32 +12,24 @@ const DrumPad = ({bankArr, index, setDisplay}) => {
   })
 
   const playSound = () => {
-    audio.currentTime = 0;
-    return audio.play();
-    // const sound = document.getElementById(bankArr[index].keyTrigger);
-    // sound.currentTime = 0;
-    // sound.play();
-    // setDisplay(bankArr[index].id)
+    const sound = document.getElementById(bankArr[index].keyTrigger);
+    sound.currentTime = 0;
+    sound.play();
+    setDisplay(bankArr[index].id)
   }
 
   return (
     <button
       className='drum-pad'
       id={bankArr[index].id}
-      onClick={() => {
-        playSound().then(() => {
-          console.log('success');
-        }).catch(error => {
-          console.log('nope');
-          console.log(error);
-        })
-      }}
+      onClick={playSound}
     >
       {bankArr[index].keyTrigger} 
       <audio 
       className='clip'
       id={bankArr[index].keyTrigger} 
-      src={bankArr[index].url}>
+      src={bankArr[index].url}
+      >
       
       </audio>
     </button>
