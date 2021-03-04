fa4accc07fafe8d2df106fdfd57f2671612cb804
Derek Price <derekp098@gmail.com>
2021-03-03 20:59:08 -0800

    added header and footer divs

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import DrumPads from './components/DrumPads';
 
 function App() {
 
-  var [displayText, setDisplayText] = useState('');
+  const [displayText, setDisplayText] = useState('');
 
   return (
     <div className="App">
-      <DrumPads setDisplay={(id) => setDisplayText(displayText = id)} />
+      <h1 className='header' >Drum Machine</h1>
+      <DrumPads setDisplay={(id) => setDisplayText(id)} />
       <Display text={displayText} />
+      <div className='footer' >
+        <p className='footer-item'>Made by Derek Price</p>
+        <a className='footer-item' href="https://github.com/DerekPixel/drum-machine">GitHub</a>
+      </div>
     </div>
   );
 }
